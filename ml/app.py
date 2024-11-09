df29fd74fe7919d2512d2e7c92824094ff236bed
RAZVAN24RR <razvanandreipasaran@gmail.com>
2024-11-09 15:20:48 +0200

    SERVE MODEL

--- ml/app.py
+++ ml/app.py
@@ -1,12 +1,17 @@
 from flask import Flask, request, jsonify
 from ml.ai_module.serve_model import serve_openai
+
 app = Flask(__name__)
 
-@app.post('/predict')
+@app.route('/predict', methods=['GET'])
 def make_prediction():
-    data = request.get_json()
+    if request.is_json:
+        data = request.get_json()
+    else:
+        data = None
+
     prediction = serve_openai()
-    return jsonify(({'prediction': prediction}))
+    return jsonify({'prediction': prediction})
 
 if __name__ == '__main__':
     app.run(debug=True)
