8e8745f84b916f4f8795012abeba103816ef8a53
RaduTek <me@radutek.uk>
2024-11-09 17:11:09 +0200

    add TestPage component

---
+++ web/src/app/test/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function TestPage() {
+    return (
+        // eslint-disable-next-line @next/next/no-img-element
+        <img
+            src="https://media.tenor.com/WKWonIB6gjIAAAAM/police-siren-siren.gif"
+            alt="Police Siren"
+        />
+    );
+}
