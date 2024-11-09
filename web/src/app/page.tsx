aed9e98e6599ad967b9c790e7051b5ce6474f91e
RaduTek <me@radutek.uk>
2024-11-09 15:07:03 +0200

    implement layout with Material-UI AppBar and navigation links

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,20 +1,63 @@
-import Image from "next/image";
+import Link from "next/link";
 import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+    AppBar,
+    Toolbar,
+    Typography,
+    Container,
+    Box,
+    Paper,
+    List,
+    ListItemButton,
+    ListItemText,
+} from "@mui/material";
+
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return (
-    <>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </>
-  );
+    return (
+        <>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{ flexGrow: 1 }}
+                    >
+                        EmergeAI
+                    </Typography>
+                    <SignedOut>
+                        <SignInButton />
+                    </SignedOut>
+                    <SignedIn>
+                        <UserButton />
+                    </SignedIn>
+                </Toolbar>
+            </AppBar>
+            <Container>
+                <Box my={4}>
+                    <Typography variant="h4" component="h1" gutterBottom>
+                        Welcome to EmergeAI
+                    </Typography>
+                    <Typography variant="body1">
+                        This is a sample page content. You can add more
+                        components and content here.
+                    </Typography>
+
+                    <Paper>
+                        <List>
+                            <ListItemButton component={Link} href="/dataentry">
+                                <ListItemText primary="Data Entry" />
+                            </ListItemButton>
+                            <ListItemButton component={Link} href="/livestatus">
+                                <ListItemText primary="Live Status" />
+                            </ListItemButton>
+                            <ListItemButton component={Link} href="/reception">
+                                <ListItemText primary="Reception" />
+                            </ListItemButton>
+                        </List>
+                    </Paper>
+                </Box>
+            </Container>
+        </>
+    );
 }
