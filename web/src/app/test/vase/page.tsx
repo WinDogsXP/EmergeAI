36f627058d0b1c98301d0d1fe70735a14809c0a2
RaduTek <me@radutek.uk>
2024-11-10 13:46:07 +0200

    feat: add VasePage component for displaying vase information

---
+++ web/src/app/test/vase/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const VasePage: React.FC = () => {
+    return (
+        <div>
+            <h1>Vase Page</h1>
+            <p>This is the Vase page component.</p>
+        </div>
+    );
+};
+
+export default VasePage;
